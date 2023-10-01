ba0914c4ce8de6776aa8c83098fcda8fd313f4c4
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-10-01 20:00:28 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left + dodger.offsetWidth < game.offsetWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
